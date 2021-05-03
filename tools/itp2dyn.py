ad009cc49725d5eef6ed543399723d492091bbde
boneta <srboneta@gmail.com>
2021-05-03 13:41:12 +0200

    Add duplicates removal, find missing, empty param

--- tools/itp2dyn.py
+++ tools/itp2dyn.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 # Description: Convert parameters from GROMACS' .itp (AMBER/GAFF) to DYNAMO's .ff (OPLS)
-# Last update: 26-04-2021
+# Last update: 03-05-2021
 
-# Inspired from ParmEd (gromacstop.py)
-# https://github.com/ParmEd/ParmEd
 
 import argparse
 import fileinput
@@ -21,6 +19,8 @@ from amber2opls import DihedralParam
 ##  INFO  #############################################################
 
 """
+    From ParmEd (gromacstop.py) https://github.com/ParmEd/ParmEd
+
     Gromacs uses "funct" flags in its parameter files to indicate what kind of
     functional form is used for each of its different parameter types. This is
     taken from the topdirs.c source code file along with a table in the Gromacs
@@ -78,7 +78,7 @@ ptable_simple_inv = { n:elem for elem, n in ptable_simple.items() }
 class DynamoTopology():
     """
         fDynamo force field topology
-    
+
         Attributes
         ----------
         top : dict of dict
@@ -130,7 +130,7 @@ class DynamoTopology():
             impropers : dict of dict(tuple)
                 v : list
                 comment : str
-    
+
     """
 
     statements_def = {'mm_definitions' : "OPLS_AA 1.0",
@@ -146,6 +146,8 @@ class DynamoTopology():
         # comprehensive topology
         self.top = {key: None for key in self.statements}
         self.top.update({key: dict() for key in self.sections})
+        # missing parameters
+        self.missing = {'atomtypes':{}, 'bonds':{}, 'angles':{}, 'dihedrals':{}, 'impropers':{}}
         # raw topology (formatted)
         self.raw_top = []
         self.raw_ndx = {key:[0]*2 for key in self.sections}
@@ -174,7 +176,7 @@ class DynamoTopology():
     @property
     def residues(self) -> list:
         return list(self.top['residues'].keys())
-    
+
     def union(self, other:'DynamoTopology', section:str=None, overwrite:bool=True) -> 'DynamoTopology':
         """Combine to topology classes"""
         for sect in self.sections:
@@ -184,6 +186,7 @@ class DynamoTopology():
             else:
                 for name, param in other.top[sect].items():
                     self.top[sect][name] = self.top[sect].get(name, param)
+        self.remove_duplicates(section, overwrite)
         return self
 
     def difference(self, other:'DynamoTopology', section:str=None) -> 'DynamoTopology':
@@ -385,6 +388,32 @@ class DynamoTopology():
                 top['nbonds'] = len(top['bonds'])
                 top['nimpropers'] = len(top['impropers'])
 
+    def remove_duplicates(self, section:str=None, overwrite:bool=True) -> None:
+        """Remove duplicate entries of bonds/angles/dihedrals due to inverted definitions"""
+        for sect in ('bonds', 'angles', 'dihedrals'):
+            if section and section != sect: continue
+            keys = list(self.top[sect].keys())
+            for key in keys:
+                yek = key[::-1]
+                if yek in keys:
+                    if overwrite: self.top[sect][key] = self.top[sect][yek]
+                    self.top[sect].pop(yek)
+                    keys.remove(yek)
+
+    def find_missing(self) -> dict:
+        """Find missing paramters from residues (atomtypes/bonds/impropers)"""
+        self.missing = {'atomtypes':{}, 'bonds':{}, 'angles':{}, 'dihedrals':{}, 'impropers':{}}
+        # atomtypes
+        atomtypes_res = {p_atom['atomtype'] for resname, p_res in self.top['residues'].items() for atom, p_atom in p_res['atoms'].items()}
+        self.missing['atomtypes'] = atomtypes_res - set(self.top['atomtypes'].keys())
+        # bonds / dihedrals
+        for sect in ('bonds', 'impropers'):
+            param_res = {name for resname, p_res in self.top['residues'].items() for name in p_res[sect]}
+            param_top = set(self.top[sect].keys())
+            if sect != 'impropers': param_top.update([name[::-1] for name in self.top[sect].keys()])
+            self.missing[sect] = param_res - param_top
+        return self.missing
+
     def assign_statements_def(self, overwrite:bool=False) -> None:
         """Assign default statements to topology"""
         for i in self.statements:
@@ -395,14 +424,13 @@ class DynamoTopology():
         self.assign_statements_def(overwrite=False)
         self.recount_top_n()
         raw = ""
-        
+
         # main header
         raw += "!"+"="*79+"\n" + \
                "!"+" "*22+"OPLS MM Definition File for Proteins\n" + \
                "!"+"="*79+"\n\n"
         raw += self.form_field('mm_definitions') + "\n\n"
 
-        
         # atomtypes
         if self.top['atomtypes']:
             raw += "!"+"="*79+"\n" + \
@@ -415,7 +443,7 @@ class DynamoTopology():
             for sect in ('electrostatics', 'lennard_jones', 'units'):
                 raw += self.form_field(sect)
             raw += "\n\n"
-        
+
         # residues / variants / links
         for sect in ('residues', 'variants', 'links'):
             if self.top[sect]:
@@ -579,6 +607,27 @@ class DynamoTopology():
             elif keyword == 'end' and current_sect is not None:
                 self.raw_ndx[current_sect][1] = n
                 current_sect = None
+
+    @staticmethod
+    def empty_param(section:str, **kwargs) -> dict:
+        """Return a empty parameter of a specified section"""
+        if section == 'atomtypes':
+            param = {'atnum':0, 'sigma':0.0, 'epsilon':0.0, 'comment':""}
+        elif section == 'residues':
+            param = {'natoms':0, 'nbonds':0, 'nimpropers':0, 'atoms':dict(), 'bonds':[], 'impropers':[]}
+        elif section in ('variants', 'links'):
+            param = {'ndeletes':0, 'nadds':0, 'ncharges':0, 'nbonds':0, 'nimpropers':0,
+                     'deletes':[], 'adds':dict(), 'charges':[], 'bonds':[], 'impropers':[]}
+        elif section == 'bonds':
+            param = {'k':0.0, 'r':0.0, 'comment':""}
+        elif section == 'angles':
+            param = {'k':0.0, 'theta':0.0, 'comment':""}
+        elif section in ('dihedrals', 'impropers'):
+            param = {'v':[0.0]*5, 'comment':""}
+        else:
+            raise ValueError("Unknown section")
+        param.update(kwargs)    # assign known arguments
+        return param
 
 
 ##  GROMACS TOPOLOGY CLASS  ###########################################
@@ -820,11 +869,11 @@ class GMXTopology():
         param = DihedralParam()
         for molname, molec in self.moleculetype.items():
             # atoms in residue
-            top['residues'][molname] = {'natoms':0, 'nbonds':0, 'nimpropers':0, 'atoms':dict(), 'bonds':[], 'impropers':[]}
+            top['residues'][molname] = DynamoTopology.empty_param('residues')
             for nr, atom in molec['atoms'].items():
                 if atom['name'] in top['residues'][molname]['atoms']:
                     raise ValueError(f"opls conversion -> duplicated atom definition (residue {molname}, atom {atom['name']})")
-                top['residues'][molname]['atoms'][atom['name']] = {'atomtype':atom['attype'].upper(), 'charge':atom['charge']} 
+                top['residues'][molname]['atoms'][atom['name']] = {'atomtype':atom['attype'].upper(), 'charge':atom['charge']}
             # bonds
             for atoms, bond in molec['bonds'].items():
                 attypes = tuple(molec['atoms'][nr]['attype'].upper() for nr in atoms)
@@ -882,12 +931,7 @@ class GMXTopology():
                     self.found.top[ptype][miss] = self.opls.top[ptype][miss[::-1]].copy()
                 else:
                     sys.stderr.write(f"WARNING: Parameter not found ({ptype[0]}) ->  {'  '.join(miss)}\n")
-                    if ptype == 'bonds':
-                        self.found.top[ptype][miss] = {'k':0., 'r':0., 'comment':"WARNING: Parameter not found."}
-                    elif ptype == 'angles':
-                        self.found.top[ptype][miss] = {'k':0., 'theta':0., 'comment':"WARNING: Parameter not found."}
-                    elif ptype in ('dihedrals', 'impropers'):
-                        self.found.top[ptype][miss] = {'v':[0.]*4, 'theta':0., 'comment':"WARNING: Parameter not found."}
+                    self.found.top[ptype][miss] = DynamoTopology.empty_param(ptype, comment=" WARNING: Parameter not found")
 
     def write_dynamo(self, file_out:str=None, only_missing:bool=False, ff_file:Union[str,list]=None) -> None:
 
