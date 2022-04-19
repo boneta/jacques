730253675b3ca72b5d98d84bba31d4ec0007bb2f
boneta <boneta@unizar.es>
2022-04-19 12:21:44 +0200

    Improve update script

--- jacques/update.sh
+++ jacques/update.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 
-# Description: Update JACQUES software from GitHub
-# Last update: 28-12-2020
+# Update JACQUES and DYNAMON software from GitHub
 
 # ask for confirmation
 read -p "Update JACQUES and DYNAMON software? (y/n): " response
 
-if [ "${response,,}" == "y" ] || [ "${response,,}" == "yes" ]; then
-  workdir=$PWD
-  cd $JACQUES
-
+if [ "${response,,}" == "y" -o "${response,,}" == "yes" ]; then
   # clean all compiled files of DYNAMON and fDynamo
   printf "$(tput setaf 5)Cleaning $(tput setaf 3; tput bold)DYNAMON$(tput sgr0) \n"
-  make clean_all -C dynamon/src
+  make clean_all -C $JACQUES/dynamon/src
 
   # update JACQUES
   printf "$(tput setaf 5)Updating $(tput setaf 3; tput bold)JACQUES$(tput sgr0) \n"
-  git fetch --all
-  git reset --hard origin/master
+  git -C $JACQUES fetch --all
+  git -C $JACQUES reset --hard origin/master
 
   # update DYNAMON
   printf "$(tput setaf 5)Updating $(tput setaf 3; tput bold)DYNAMON$(tput sgr0) \n"
-  git submodule update --init --recursive
+  git -C $JACQUES submodule update --init --recursive
 
   # compile DYNAMON
   printf "$(tput setaf 5)Compiling $(tput setaf 3; tput bold)DYNAMON$(tput sgr0) \n"
-  make -C dynamon/src
-
-  cd $workdir
+  make -C $JACQUES/dynamon/src
 else
   printf "Update cancelled. \n"
-  exit
+  exit 1
 fi
